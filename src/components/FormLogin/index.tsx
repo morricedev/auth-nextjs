b2094137e868311c7e5fea9c96375a9ecd6897c0
Maurício Sousa <morricedev@gmail.com>
2021-08-05 09:37:08 -0300

    Create FormLogin component

---
+++ src/components/FormLogin/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Email } from '@styled-icons/material-outlined/Email';
+import { Password } from '@styled-icons/material-outlined/Password';
+
+import { TextInput } from '../TextInput';
+import * as Styled from './styles';
+import { Button } from '../Button';
+
+export type FormLoginProps = {
+  errorMessage?: string;
+  onLogin?: (email: string, password: string) => Promise<void>;
+};
+
+export const FormLogin = ({ errorMessage, onLogin }: FormLoginProps) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
+
+    if (onLogin) {
+      await onLogin(email, password);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Styled.Wrapper onSubmit={handleSubmit}>
+      <TextInput
+        type="email"
+        name="user-identifier"
+        label="Seu e-mail"
+        onInputChange={(v) => setEmail(v)}
+        value={email}
+        icon={<Email />}
+        required
+      />
+      <TextInput
+        type="password"
+        name="user-password"
+        label="Sua senha"
+        onInputChange={(v) => setPassword(v)}
+        value={password}
+        icon={<Password />}
+        required
+      />
+
+      {!!errorMessage && (
+        <Styled.ErrorMessage>{errorMessage}</Styled.ErrorMessage>
+      )}
+
+      <Styled.ButtonWrapper>
+        <Button disabled={loading}>{loading ? 'Aguarde...' : 'Entrar'}</Button>
+      </Styled.ButtonWrapper>
+    </Styled.Wrapper>
+  );
+};
