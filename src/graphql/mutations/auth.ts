d6e09ba6eaf4714e0b3e6c8f0acf28366e493913
Maurício Sousa <morricedev@gmail.com>
2021-08-07 14:52:41 -0300

    Add mutations for register & forgot/reset password

--- src/graphql/mutations/auth.ts
+++ src/graphql/mutations/auth.ts
@@ -12,3 +12,49 @@ export const GQL_MUTATION_AUTHENTICATE_USER = gql`
     }
   }
 `;
+
+export const GQL_MUTATION_REGISTER_USER = gql`
+  ${GQL_FRAGMENT_USER}
+  mutation REGISTER_USER(
+    $email: String!
+    $username: String!
+    $password: String!
+  ) {
+    register(
+      input: { email: $email, username: $username, password: $password }
+    ) {
+      jwt
+      user {
+        ...user
+      }
+    }
+  }
+`;
+
+export const GQL_MUTATION_FORGOT_PASSWORD = gql`
+  mutation FORGOT_PASSWORD($email: String!) {
+    forgotPassword(email: $email) {
+      ok
+    }
+  }
+`;
+
+export const GQL_MUTATION_RESET_PASSWORD = gql`
+  ${GQL_FRAGMENT_USER}
+  mutation RESET_PASSWORD(
+    $code: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    resetPassword(
+      code: $code
+      password: $password
+      passwordConfirmation: $confirmPassword
+    ) {
+      jwt
+      user {
+        ...user
+      }
+    }
+  }
+`;
